da6826ec298992bea3c96faff699b00fa035c120
Chris Morris <44758151+chriswmorris@users.noreply.github.com>
2018-11-29 21:03:05 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Run as root
+
+echo "========================="
+echo "Running Update"
+echo "========================="
+sleep 1
+sudo apt update
+
+sleep .5
+echo "========================="
+echo "Running Upgrade"
+echo "========================="
+sleep .5
+sudo apt -y upgrade
+
+sleep .5
+echo "========================="
+echo "Running Dist-Upgrade"
+echo "========================="
+sleep .5
+sudo apt -y dist-upgrade
+
+sleep .5
+echo "============================="
+echo "Running Autoremove and Clean"
+echo "============================="
+sleep .5
+sudo apt -y autoremove
+sudo apt clean
